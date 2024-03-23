2380f996cd6c984f2480fddccabdf5d4a9b3ff38
lmioduszewski <luke.mioduszewski@gmail.com>
2024-03-22 22:11:46 -0700

    added bokeh fig

--- bokeh_fig.py
+++ bokeh_fig.py
@@ -4,11 +4,16 @@ import bokeh.plotting as bkp
 class BokehFig:
     def __init__(self, *args, **kwargs):
 
+        print('inited')
         self.f = figure(*args, **kwargs)
-        self.f.sizing_mode = 'stretch_both'
+        #  self.f.sizing_mode = 'stretch_both'
+
 
     def show(self):
         return bkp.show(self.f)
+
+    def line(self, *args, **kwargs):
+        self.f.line(*args, **args)
 
 
 if __name__ == "__main__":
